e8313755a3e9f6e43e8543d95c5c92ce3332a939
shal4ph <shalvidhi@gmail.com>
2024-02-25 12:16:53 +0000

    ww

---
+++ javascript/calcTax.js
@@ -0,0 +1,17 @@
+// function calculateTax(price, taxRate){
+//    // let result = price + (taxRate/100)*price;
+
+// return parseInt(price + (taxRate/100)*price);
+// }
+
+// console.log("total price of tax is: "+calculateTax(100, 50));
+// ;
+
+let emp = { first: 'Sita', last: 'Jija', age: 29};
+let emp2 = { first: 'Mita', last: 'Pija', age: 49};
+
+let showDetails = function(username){
+    console.log(this.first+ " "+ this.last+ " with age as "+this.age+ " has username "+ username);
+}
+showDetails.call(emp2, "shal1234");
+showDetails.apply(emp, ['shalu89']);
