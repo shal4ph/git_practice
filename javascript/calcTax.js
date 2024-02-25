f744895fcb6e8ae81e5f630e6f1459ac27ea12ab
shal4ph <shalvidhi@gmail.com>
2024-02-25 12:18:06 +0000

    ww

--- javascript/calcTax.js
+++ javascript/calcTax.js
@@ -10,8 +10,8 @@
 let emp = { first: 'Sita', last: 'Jija', age: 29};
 let emp2 = { first: 'Mita', last: 'Pija', age: 49};
 
-let showDetails = function(username){
-    console.log(this.first+ " "+ this.last+ " with age as "+this.age+ " has username "+ username);
+let showDetails = function(username, salary){
+    console.log(this.first+ " "+ this.last+ " with age as "+this.age+ " has username "+ username + " and has salary "+salary);
 }
-showDetails.call(emp2, "shal1234");
-showDetails.apply(emp, ['shalu89']);
+showDetails.call(emp2, "shal1234", "1000");
+showDetails.apply(emp, ['shalu89', '909']);
