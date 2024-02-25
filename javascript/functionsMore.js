65cb1b9221c70074908a94222c4fa365d0cbccf7
shal4ph <shalvidhi@gmail.com>
2024-02-25 12:46:53 +0000

    ww

--- javascript/functionsMore.js
+++ javascript/functionsMore.js
@@ -1,15 +1,34 @@
-function greetUser(hours){
-    if (hours>=0 && hours <=12){
-return "good morn";
-    }
-    else if (hours>=12 && hours <=16){
-        return "good aft";
+// function greetUser(hours){
+//     if (hours>=0 && hours <=12){
+// return "good morn";
+//     }
+//     else if (hours>=12 && hours <=16){
+//         return "good aft";
         
+//     }
+//     else if (hours >=16 && hours <=20){
+//         return "good nigt";
+//     }
+// }
+// console.log(greetUser(10));
+// console.log(greetUser(15));
+// console.log(greetUser(20));
+
+let orderData1 = {name:"shampu", cost: 500.0, discount: 10, taxrate:10, inStock: true, calculatePrice: function(){
+    return (this.cost + (this.taxrate*100)/this.cost - (this.discount*100)/this.cost);
+},
+
+displayInstock: function(productName){
+    if(this.inStock){
+        return productName+ " is in stk";
     }
-    else if (hours >=16 && hours <=20){
-        return "good nigt";
-    }
+    else {return productName = " is not in stk";}
+},
+showDiscount: function() {
+    return "tot discount is "+this.discount
 }
-console.log(greetUser(10));
-console.log(greetUser(15));
-console.log(greetUser(20));
+
+}
+console.log(orderData1.calculatePrice());
+console.log(orderData1.displayInstock());
+console.log(orderData1.showDiscount());
