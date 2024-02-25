afa54049978d8c79079d8daccb1900404aec100f
shal4ph <shalvidhi@gmail.com>
2024-02-25 10:12:36 +0000

    ww

---
+++ javascript/loops.js
@@ -0,0 +1,14 @@
+//loop var initiasation
+//loop condition
+//loop update increment or decrement i++ i--
+
+
+// for (let i =1; i<=10; i=i+1){
+//     console.log(i);
+// }
+for (let i=10; i>=1; i=i-1){
+if(i==5){
+    continue;
+}
+console.log(i);
+}
