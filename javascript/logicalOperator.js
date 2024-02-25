f5b0dc3c7bc2e25b0f416a8834ae9d2eca1328b9
shal4ph <shalvidhi@gmail.com>
2024-02-25 10:00:45 +0000

    ww

---
+++ javascript/logicalOperator.js
@@ -0,0 +1,23 @@
+// bookRating = 2.2;
+// bookGenre = "crime";
+// isComplete = false;
+
+
+// // if(bookRating>4.0 && bookGenre == "crime"){
+// // console.log("buy book");
+
+// // } else {
+// //     console.log("do not buy");
+// // }
+
+// console.log(!isComplete);
+// console.log(5**5);
+
+let a = prompt("enter 1st no :");
+let b = prompt("enter 2nd no :");
+let c = prompt("enter 3rd no :");
+if(a**2+b**2 ==c**2){
+    alert("pythagorean triple yes");
+} else {
+    alert("not pythagorean triple");
+}
