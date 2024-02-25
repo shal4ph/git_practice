d9716f782d1edf39ebda3abc6197f32817e0bb4a
shal4ph <shalvidhi@gmail.com>
2024-02-25 09:12:03 +0000

    ww

---
+++ javascript/ifelse.js
@@ -0,0 +1,13 @@
+let username = "juhis@gmail.com";
+let password = "ggg"
+if(username == "juhi@gmail.com"){
+    if(password =="ggg"){
+        console.log("username and pwd right");
+    }
+    else {
+        console.log("username right but password wrong");
+    }
+
+} else {
+   console.log("nah username wrong");
+}
