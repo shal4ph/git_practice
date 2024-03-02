9a241beb7146b74112daf43568a3b350ecf54750
shal4ph <shalvidhi@gmail.com>
2024-03-02 10:23:24 +0000

    objects arrays

--- javascripts_advanced/object.js
+++ javascripts_advanced/object.js
@@ -11,3 +11,5 @@ console.log(person);
 console.log(person.company);
 person.name = "qqqQQ";
 console.log(person.name);
+console.log(person.name + " is " + person.age + " years old");
+console.log(`${person.name} is ${person.age} years old`);
