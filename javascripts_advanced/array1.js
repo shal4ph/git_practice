fdbd6fdfb37a274faff3e2a1948b9a5d871782f9
shal4ph <shalvidhi@gmail.com>
2024-03-02 09:32:57 +0000

    objects arrays

--- javascripts_advanced/array1.js
+++ javascripts_advanced/array1.js
@@ -1,4 +1,10 @@
-let allReviews = ["one", "two", "ww"];
-console.log("before reverse: " + allReviews);
-allReviews.reverse();
-console.log("before reverse: " + allReviews);
+// let allReviews = ["one", "two", "ww"];
+// console.log("before reverse: " + allReviews);
+// allReviews.reverse();
+// console.log("before reverse: " + allReviews);
+
+
+let prices = [100.2, 200.6, 350.7];
+let newPrices = Array.of(100.2, 200.6, 350.7);
+console.log(newPrices);
+console.log(prices);
