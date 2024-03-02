5c601cc5ef0c3d7c0819455ebdadf7e43493a5e2
shal4ph <shalvidhi@gmail.com>
2024-03-02 08:33:14 +0000

    objects arrays

--- javascript/functionsMore.js
+++ javascript/functionsMore.js
@@ -16,6 +16,10 @@
 
 let orderData1 = {name:"shampu", cost: 500.0, discount: 10, taxrate:10, inStock: true, calculatePrice: function(){
     return (this.cost + (this.taxrate*100)/this.cost - (this.discount*100)/this.cost);
+},}
+
+let orderData2 = {name:"shamp999u", cost: 50990.0, discount: 9, taxrate:50, inStock: false, calculatePrice: function(){
+    return (this.cost + (this.taxrate*100)/this.cost - (this.discount*100)/this.cost);
 },
 
 displayInstock: function(productName){
@@ -26,9 +30,13 @@ displayInstock: function(productName){
 },
 showDiscount: function() {
     return "tot discount is "+this.discount
-}
+},
 
 }
+
 console.log(orderData1.calculatePrice());
-console.log(orderData1.displayInstock());
+console.log(orderData1.displayInstock("shampu"));
 console.log(orderData1.showDiscount());
+
+
+orderData1.calculatePrice.call(orderData2);
