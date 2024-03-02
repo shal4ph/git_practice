b9499cd2cb547a1f820c6900f5766b6bf691c054
shal4ph <shalvidhi@gmail.com>
2024-03-02 10:12:28 +0000

    objects arrays

--- javascripts_advanced/array1.js
+++ javascripts_advanced/array1.js
@@ -4,7 +4,40 @@
 // console.log("before reverse: " + allReviews);
 
 
-let prices = [100.2, 200.6, 350.7];
-let newPrices = Array.of(100.2, 200.6, 350.7);
-console.log(newPrices);
-console.log(prices);
+// let prices = [100.2, 200.6, 350.7];
+// let newPrices = Array.of(100.2, 200.6, 350.7);
+// console.log(newPrices);
+// console.log(prices);
+
+// let characters = Array.from("How are you?");
+// console.log(characters);
+// console.log(characters.join(""));
+
+
+let numbers = [];
+let choice;
+do{
+console.log("choose option: \n1. add a no\n2. remove a no\n3. print nos\n4. exit")
+choice = parseInt(prompt("enter your choice: "));
+switch(choice){
+
+    case 1: let numberToAdd = prompt("enter value to add: "); 
+    numbers.push(numberToAdd); console.log("added");
+    break;
+
+    case 2: let numberToRemove = prompt("enter no to remove: ");
+    let indexToRemove = numbers.indexOf(numberToRemove);
+    if (indexToRemove !== -1){
+        number.splice(indexToRemove, 1);
+        console.log("removed");
+    }
+        else {console.log("no such value found")}
+    
+    break;
+    case 3: for (const number of numbers){console.log(number)};break;
+    case 4: console.log("thanks for coming"); break;
+    default: console.log("invalid choice")
+;}
+}
+while (choice >=1 && choice < 4);
+
---
+++ javascripts_advanced/object.js
@@ -0,0 +1,13 @@
+let person = {
+
+name: "ww",
+isEmployed: false,
+age: 34,
+
+}
+console.log(person.name);
+person.company = "QQ";
+console.log(person);
+console.log(person.company);
+person.name = "qqqQQ";
+console.log(person.name);
