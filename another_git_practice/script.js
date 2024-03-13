7d447b0beec3992c64c3ebe4f3684cd4b7c3809b
shal4ph <shalvidhi@gmail.com>
2024-03-13 22:29:28 +0000

    hehe

---
+++ another_git_practice/script.js
@@ -0,0 +1,10 @@
+let postTitle = document.querySelector("h1");
+postTitle.addEventListener('mouseover', function() {
+
+postTitle.style.backgroundColor = "yellow";
+});
+postTitle.addEventListener('mouseout', function() {
+
+    postTitle.style.backgroundColor = "gray";
+    });
+// console.log(postTitle);
