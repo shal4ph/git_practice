892223e6ebe275b734c408ef1020492674d8b8c6
shal4ph <shalvidhi@gmail.com>
2024-03-16 13:37:13 +0000

    objects arrays

--- another_git_practice/script.js
+++ another_git_practice/script.js
@@ -9,9 +9,19 @@
 //     });
 // // console.log(postTitle);
 
-let blogTitle = document.getElementById("blogTitle");
-console.log(blogTitle);
-blogTitle.addEventListener("change", (e) => {e.target.value.length==0
-    ?(blogTitle.style.border="5px solid red")
-    :(blogTitle.style.border="5px solid green");
-});
+// let blogTitle = document.getElementById("blogTitle");
+// console.log(blogTitle);
+// blogTitle.addEventListener("change", (e) => {e.target.value.length==0
+//     ?(blogTitle.style.border="5px solid red")
+//     :(blogTitle.style.border="5px solid green");
+// });
+
+let addPostButton = document.querySelector("button");
+addPostButton.addEventListener("click", ()=>{storePost});
+
+function storePost(){
+    let blogTitle = document.getElementById("blogTitle").value;
+console.log(blogTitle + " will be added!");
+
+
+}
