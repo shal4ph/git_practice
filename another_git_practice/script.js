a7ab4f17f10c8682394f2dc3b46e1e20d4d34fba
shal4ph <shalvidhi@gmail.com>
2024-03-16 13:18:38 +0000

    objects arrays

--- another_git_practice/script.js
+++ another_git_practice/script.js
@@ -1,10 +1,13 @@
-let postTitle = document.querySelector("h1");
-postTitle.addEventListener('mouseover', function() {
+// let postTitle = document.querySelector("h1");
+// postTitle.addEventListener('mouseover', function() {
 
-postTitle.style.backgroundColor = "yellow";
-});
-postTitle.addEventListener('mouseout', function() {
+// postTitle.style.backgroundColor = "yellow";
+// });
+// postTitle.addEventListener('mouseout', function() {
 
-    postTitle.style.backgroundColor = "gray";
-    });
-// console.log(postTitle);
+//     postTitle.style.backgroundColor = "transparent";
+//     });
+// // console.log(postTitle);
+
+let blogTitle = document.getElementById("blogTitle");
+blogTitle.addEventListener("change", (e) => {console.log(e.target.value)});
