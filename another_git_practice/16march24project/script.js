08bd20a9c28993d7bf912196408c79317c929c49
shal4ph <shalvidhi@gmail.com>
2024-05-06 22:24:58 +0100

    aaa..pp7

--- another_git_practice/16march24project/script.js
+++ another_git_practice/16march24project/script.js
@@ -1,8 +1,12 @@
 // https://www.googleapis.com/books/v1/volumes?q=quilting
 //step 1: read user input
+let searchButton = document.getElementById("SearchButton");
+searchButton.addEventListener("click", fetchBooks);
 
-let bookTitle = document.getElementById("bookTitle");
+function fetchBooks(){
+    let bookTitle = document.getElementById("bookTitle").value;
 console.log(bookTitle);
+}
 //step 2. create request
 //step 3 send request and received response
 //step 4 display on front end
