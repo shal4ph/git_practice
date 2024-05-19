3bf07935dab2079221037e5d4e944ac19916c26b
shal4ph <shalvidhi@gmail.com>
2024-05-19 21:39:36 +0100

    ss

--- another_git_practice/16march24project/script.js
+++ another_git_practice/16march24project/script.js
@@ -1,12 +1,51 @@
 // https://www.googleapis.com/books/v1/volumes?q=quilting
-//step 1: read user input
-let searchButton = document.getElementById("SearchButton");
+
+
+let searchButton = document.getElementById("searchButton");
 searchButton.addEventListener("click", fetchBooks);
 
-function fetchBooks(){
+let allBooks = document.getElementById("allBooks");
+
+let content = "";
+
+function fetchBooks() {
+    //step 1: read user input
     let bookTitle = document.getElementById("bookTitle").value;
-console.log(bookTitle);
+    console.log(bookTitle);
 }
 //step 2. create request
+let myRequest = new XMLHttpRequest(); //readyState = 0
+myRequest.open("GET", `https://www.googleapis.com/books/v1/volumes?q=${bookTitle}`
+); //readyState = 1 now
+
 //step 3 send request and received response
-//step 4 display on front end
+myRequest.send(); //readyState = 2
+
+//server processing, readyState = 3
+//after response, readyState = 4
+myRequest.onreadystatechange = () => {
+  
+    if (myRequest.readyState === 4 && myRequest.status === 200) {
+        let convertedBooks = JSON.parse(myRequest.response);
+        console.log(convertedBooks);
+        let newBook = convertedBooks.items
+
+        .map(
+            (book) => 
+            `<div>
+            <img src=${book.volumeInfo.imageLinks.thumbnail}/>
+                <h1>${book.volumeInfo.title}</h1>
+                <p>${book.volumeInfo.description}</p>
+            </div>`
+        )
+        .join("");
+
+        content += newBook;
+     
+    }
+    allBooks.innerHTML = content;
+};
+
+//step 4 convert response to JS object
+
+//step 5 - display on front end
