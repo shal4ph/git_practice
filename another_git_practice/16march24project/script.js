9c3d4d6e506e5ffc7e3a6f347c01ecea6f39d4d7
shal4ph <shalvidhi@gmail.com>
2024-05-19 22:04:01 +0100

    ss

--- another_git_practice/16march24project/script.js
+++ another_git_practice/16march24project/script.js
@@ -8,31 +8,32 @@ let allBooks = document.getElementById("allBooks");
 
 let content = "";
 
-function fetchBooks() {
+async function fetchBooks() {
     //step 1: read user input
     let bookTitle = document.getElementById("bookTitle").value;
     console.log(bookTitle);
 }
 //step 2. create request
-let myRequest = new XMLHttpRequest(); //readyState = 0
-myRequest.open("GET", `https://www.googleapis.com/books/v1/volumes?q=${bookTitle}`
-); //readyState = 1 now
+// let myRequest = new XMLHttpRequest(); //readyState = 0
+// myRequest.open("GET", `https://www.googleapis.com/books/v1/volumes?q=${bookTitle}`
+// ); //readyState = 1 now
 
 //step 3 send request and received response
-myRequest.send(); //readyState = 2
+//myRequest.send(); //readyState = 2
 
 //server processing, readyState = 3
 //after response, readyState = 4
-myRequest.onreadystatechange = () => {
+// myRequest.onreadystatechange = () => {
   
-    if (myRequest.readyState === 4 && myRequest.status === 200) {
-        let convertedBooks = JSON.parse(myRequest.response);
-        console.log(convertedBooks);
+//     if (myRequest.readyState === 4 && myRequest.status === 200) {
+    let response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${bookTitle}`);
+        let convertedBooks = await response.json();
+      //  console.log(convertedBooks);
         let newBook = convertedBooks.items
 
         .map(
             (book) => 
-            `<div>
+            `<div class="book">
             <img src=${book.volumeInfo.imageLinks.thumbnail}/>
                 <h1>${book.volumeInfo.title}</h1>
                 <p>${book.volumeInfo.description}</p>
@@ -42,9 +43,9 @@ myRequest.onreadystatechange = () => {
 
         content += newBook;
      
-    }
+    //}
     allBooks.innerHTML = content;
-};
+//};
 
 //step 4 convert response to JS object
 
