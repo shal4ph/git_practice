ac28589f267e725c9db311c09b08a5583a2f8554
shal4ph <shalvidhi@gmail.com>
2024-05-26 23:28:15 +0100

    ww

--- another_git_practice/17marchsundayproject/menu-app/src/js/index.js
+++ another_git_practice/17marchsundayproject/menu-app/src/js/index.js
@@ -1,20 +1,72 @@
+const menu = [
+
+{
+  id: 1,
+  title: "quarantine buddy",
+  category: "shakes",
+  price: 16.99,
+  img: "./images/item1.jpg",
+  desc: `hello this is shake.`,
+},
+{
+  id: 2,
+  title: " buddy",
+  category: "dinner",
+  price: 10.99,
+  img: "./images/item1.jpg",
+  desc: `hello this is dinner.`,
+},
+,
+{
+  id: 3,
+  title: "quarantineyy buddy",
+  category: "shakes",
+  price: 11.99,
+  img: "./images/item1.jpg",
+  desc: `hello this is shake.`,
+},
+{
+  id: 4,
+  title: "quarant buddy",
+  category: "lunch",
+  price: 16.99,
+  img: "./images/item1.jpg",
+  desc: `hello this is lunch.`,
+},
+{
+  id: 5,
+  title: "qua buddy",
+  category: "lunch",
+  price: 19.99,
+  img: "./images/item1.jpg",
+  desc: `hello this is lunch.`,
+},
+{
+  id: 6,
+  title: "quyy buddy",
+  category: "breakfast",
+  price: 14.99,
+  img: "./images/item1.jpg",
+  desc: `hello this is nashta.`,
+}
+];
+
+
+
 let allDishes = document.getElementById("allDishes");
 let content = "";;
-menubar.forEach(()=>{
+menubar.forEach((dish)=>{
   let individualDish = ` <div class="dish">
-  <img src="./images/item1.jpg" width="200" height="90" />
+  <img src=${dish.img} width="200" height="90" />
   <div class="dishOuter">
 
   <div class="dishInner">
-    <h3 class="dishName">Buttermilk Pancake</h3>
-    <h3 class="dishPrice">$10.00</h3>
+    <h3 class="dishName">${dish.title}</h3>
+    <h3 class="dishPrice">$${dish.price}</h3>
   </div>
 
   <p class="dishContent">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere quas
-    iste impedit quis, recusandae necessitatibus commodi nobis iusto amet
-    quidem quasi exercitationem doloremque placeat cumque eaque repellat.
-    Excepturi, labore? Voluptates.
+   ${dish.desc}
   </p>
 
 </div>
