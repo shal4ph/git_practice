0973c0d18c3f887d210c9789bf38617fcfc7199d
shal4ph <shalvidhi@gmail.com>
2024-05-27 13:19:19 +0100

    ww

--- another_git_practice/17marchsundayproject/menu-app/src/api/routes.js
+++ another_git_practice/17marchsundayproject/menu-app/src/api/routes.js
@@ -1,13 +1,61 @@
 import { rest } from 'msw';
 // For more information on creating simulated HTTP API endpoints, please visit https://mswjs.io/docs/
 export const routes = [
-  rest.get('/', (req, res, ctx) => {
+  rest.get("/dishes", (req, res, ctx) => {
     return res(
       ctx.status(200),
-      ctx.json({
-        message:
-          'App running in development mode with Mock Service Worker based API.\nPlease see src/js/index.js and src/api/routes.js for customisation',
-      })
+      ctx.json([
+
+        {
+          id: 1,
+          title: "quarantine buddy",
+          category: "shakes",
+          price: 16.99,
+          img: "./images/item1.jpg",
+          desc: `hello this is shake.`,
+        },
+        {
+          id: 2,
+          title: " buddy",
+          category: "dinner",
+          price: 10.99,
+          img: "./images/item1.jpg",
+          desc: `hello this is dinner.`,
+        },
+        ,
+        {
+          id: 3,
+          title: "quarantineyy buddy",
+          category: "shakes",
+          price: 11.99,
+          img: "./images/item1.jpg",
+          desc: `hello this is shake.`,
+        },
+        {
+          id: 4,
+          title: "quarant buddy",
+          category: "lunch",
+          price: 16.99,
+          img: "./images/item1.jpg",
+          desc: `hello this is lunch.`,
+        },
+        {
+          id: 5,
+          title: "qua buddy",
+          category: "lunch",
+          price: 19.99,
+          img: "./images/item1.jpg",
+          desc: `hello this is lunch.`,
+        },
+        {
+          id: 6,
+          title: "quyy buddy",
+          category: "breakfast",
+          price: 14.99,
+          img: "./images/item1.jpg",
+          desc: `hello this is nashta.`,
+        }
+        ])
     );
   }),
 ];
--- another_git_practice/17marchsundayproject/menu-app/src/js/index.js
+++ another_git_practice/17marchsundayproject/menu-app/src/js/index.js
@@ -1,55 +1,55 @@
-const menu = [
+// const menu = [
 
-{
-  id: 1,
-  title: "quarantine buddy",
-  category: "shakes",
-  price: 16.99,
-  img: "./images/item1.jpg",
-  desc: `hello this is shake.`,
-},
-{
-  id: 2,
-  title: " buddy",
-  category: "dinner",
-  price: 10.99,
-  img: "./images/item1.jpg",
-  desc: `hello this is dinner.`,
-},
-,
-{
-  id: 3,
-  title: "quarantineyy buddy",
-  category: "shakes",
-  price: 11.99,
-  img: "./images/item1.jpg",
-  desc: `hello this is shake.`,
-},
-{
-  id: 4,
-  title: "quarant buddy",
-  category: "lunch",
-  price: 16.99,
-  img: "./images/item1.jpg",
-  desc: `hello this is lunch.`,
-},
-{
-  id: 5,
-  title: "qua buddy",
-  category: "lunch",
-  price: 19.99,
-  img: "./images/item1.jpg",
-  desc: `hello this is lunch.`,
-},
-{
-  id: 6,
-  title: "quyy buddy",
-  category: "breakfast",
-  price: 14.99,
-  img: "./images/item1.jpg",
-  desc: `hello this is nashta.`,
-}
-];
+// {
+//   id: 1,
+//   title: "quarantine buddy",
+//   category: "shakes",
+//   price: 16.99,
+//   img: "./images/item1.jpg",
+//   desc: `hello this is shake.`,
+// },
+// {
+//   id: 2,
+//   title: " buddy",
+//   category: "dinner",
+//   price: 10.99,
+//   img: "./images/item1.jpg",
+//   desc: `hello this is dinner.`,
+// },
+// ,
+// {
+//   id: 3,
+//   title: "quarantineyy buddy",
+//   category: "shakes",
+//   price: 11.99,
+//   img: "./images/item1.jpg",
+//   desc: `hello this is shake.`,
+// },
+// {
+//   id: 4,
+//   title: "quarant buddy",
+//   category: "lunch",
+//   price: 16.99,
+//   img: "./images/item1.jpg",
+//   desc: `hello this is lunch.`,
+// },
+// {
+//   id: 5,
+//   title: "qua buddy",
+//   category: "lunch",
+//   price: 19.99,
+//   img: "./images/item1.jpg",
+//   desc: `hello this is lunch.`,
+// },
+// {
+//   id: 6,
+//   title: "quyy buddy",
+//   category: "breakfast",
+//   price: 14.99,
+//   img: "./images/item1.jpg",
+//   desc: `hello this is nashta.`,
+// }
+// ];
 
 
 
